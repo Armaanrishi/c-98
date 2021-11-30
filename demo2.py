012f4e8cff334da939113355628b508492b5c79a
Armaanrishi <80962650+Armaanrishi@users.noreply.github.com>
2021-11-30 19:37:36 +0530

    Add files via upload

---
+++ demo2.py
@@ -0,0 +1,3 @@
+introString = "My name is Armaan, I am 14 years old."
+words = introString.split(",")
+print (words)
---
+++ demo5.py
@@ -0,0 +1,5 @@
+def myfunc(num):
+    while num>0:
+        print("Hello")
+        num=num-1
+myfunc(4)
